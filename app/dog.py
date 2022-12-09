0134722f3839e623eb60f3663362f6ee8be0a1ad
Malcome Fein <mfein90@gmail.com>
2022-12-09 12:10:02 -0500

    Done.

--- app/dog.py
+++ app/dog.py
@@ -1,27 +1,33 @@
 from sqlalchemy import create_engine
-
+from models import Dog
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
-    pass
+    base.metadata.create_all(engine)
+    return engine
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
 
 def new_from_db(session):
-    pass
+    return session.query(Dog).first()
 
 def get_all(session):
-    pass
+    return session.query(Dog)#.all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
+    ## try with filter!!!! 
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name).filter_by(breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).update({
+        Dog.breed: breed
+    })
